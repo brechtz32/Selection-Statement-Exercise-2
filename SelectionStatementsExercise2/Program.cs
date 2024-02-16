db241378a1e9279c850d15eb0bfa6ea27329865b
zach brecht <brechtz32@gmail.com>
2024-02-15 19:06:14 -0600

    first commit of selectionstatementsexercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Console.WriteLine("What is your favorite school subject?");
+
+        string subject = Console.ReadLine();
+
+        switch(subject.ToLower())
+        {
+            case "math":
+                Console.WriteLine("You must be good with numbers!");
+                break;
+            case "science":
+                Console.WriteLine("Experiements are fun!");
+                break;
+            case "reading":
+                Console.WriteLine("I bet you've read a lot of books!");
+                break;
+            case "history":
+                Console.WriteLine("You must know about a lot of famous people!");
+                break;
+            case "health":
+                Console.WriteLine("You probably know a lot about nutrition!");
+                break;
+            default:
+                Console.WriteLine("That subject is not an option.");
+                break;
+
+
+        }
+
         }
     }
 }
